d5a41e3b097eafdb4f265cb56e023a4f19ed10e0
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-05-09 21:23:10 +0100

    :bug: :sparkles: First working version!
    stores device serial and relay id as settings.
    Device and be selected and turned on and off.
    
    WARNING: This is still a work in progress!

--- usb-hid-relay.js
+++ usb-hid-relay.js
@@ -1,26 +1,53 @@
 module.exports = function(RED) {
     "use strict";
 
-    const USBRelay = require("@josephdadams/usbrelay"); 
+    console.log("test");
+    const USBRelay = require("usbrelay"); 
 
+    let relayNodeName = function(relay){
+        
+    }
+
+    
     function RelayNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         
-        var path = relay.device.path;
-        node.relay = new USBRelay(config.path);
+        var portInfo = config.usbrelayport.split("_"); //relay.device.path;
+        let allRelays = USBRelay.Relays;
+        
+        if(portInfo.length==2){
+            let device = allRelays.find( (device)=>device.serial==portInfo[0] );
+            if(device){
+                node.port = parseInt(portInfo[1]);
+                node.relay = new USBRelay(device.path);            
+            }
+        }
+        else{
+            try{
+                node.relay = new USBRelay();
+            }
+            catch(e){
+
+            }
+        }
+        
         
         node.on('input', function(msg) {
             //node.send(msg);
-            relay.setState(1, !!msg.payload );
+            if(node.relay)
+                node.relay.setState(node.port+1, !!msg.payload );
         });
 
         RED.httpAdmin.get('/usbrelays/' + node.id + '/current', function(req, res, next) {
-            res.end(JSON.stringify(path));
+            if(node.relay)
+                res.end(JSON.stringify(node.relay.getSerialNumber()+"_"+node.port));
+            else 
+                res.send(JSON.stringify(""));
             return;
         });
     }
-    RED.nodes.registerType("relay node",RelayNode);
+    RED.nodes.registerType("usb relay",RelayNode);
 
     RED.httpAdmin.get('/usbrelays', function(req, res, next) {        
         res.end(JSON.stringify(USBRelay.Relays));
