1be17f5d924d0e26fa053c61dbdea40e61a365c0
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-09-23 08:34:08 +0100

    Updated project dependancies to use new usbrelay node

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // the following should throw an error is there's an issue with the usbrelay library
-const USBRelay = require("USBRelay"); 
+const USBRelay = require("@josephdadams/usbrelay"); 
 
 relayList = USBRelay.Relays;
 
--- usb-hid-relay.js
+++ usb-hid-relay.js
@@ -1,7 +1,7 @@
 module.exports = function(RED) {
     "use strict";
 
-    const USBRelay = require("USBRelay"); 
+    const USBRelay = require("@josephdadams/usbrelay"); 
 
     const settings = {
         connectionCheckTime : 5*1000
